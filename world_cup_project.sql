707825443f6fefb579d7f759c660c2e297ad8318
jar-data <jaredconnermoss@gmail.com>
2024-07-08 14:31:29 -0400

    Update world_cup_project.sql

--- world_cup_project.sql
+++ world_cup_project.sql
@@ -89,3 +89,55 @@ SELECT team, sum(xg)
 		WHERE Date > '2018-01-01') AS sq
 GROUP BY team
 ORDER BY 2 DESC
+	
+
+
+-- Creating view to show WC wins by association
+
+USE WorldCupProject
+GO
+CREATE VIEW wins-by-association AS
+SELECT rank.association, COUNT(*) AS wins_by_assoc
+FROM WorldCupProject..fifa_ranking AS rank
+JOIN WorldCupProject..world_cup AS cup
+ON rank.team = cup.champion
+GROUP BY rank.association;
+
+
+-- Creating a view showing average attendance at all WCs since 1930
+
+USE WorldCupProject
+GO
+CREATE VIEW avg_attendance AS
+SELECT Year, (Attendance/Matches) as AverageAttendance
+FROM WorldCupProject..world_cup
+ORDER BY 1
+
+
+	-- Creating a view that shows the # of goals by WC 
+
+USE WorldCupProject
+GO
+CREATE VIEW goals-per-WC AS
+SELECT Year, Host, home_goals+away_goals as total_goals, home_penalties+away_penalties as total_penalties
+FROM (
+	SELECT Year, Host, SUM(home_score) as home_goals, SUM(away_score) as away_goals, SUM(home_penalty) as home_penalties, SUM(away_penalty) as away_penalties
+	FROM WorldCupProject..matches
+	GROUP BY Year, Host) AS sq
+	ORDER BY 1
+
+
+-- Creating a view that shows WC goals by team
+
+USE WorldCupProject
+GO
+CREATE VIEW goals_per_team AS
+	SELECT team, sum(total_goals) as total_goals
+	FROM(
+		SELECT home_score AS total_goals, home_team AS team 
+		FROM WorldCupProject..matches
+		UNION ALL 
+		SELECT away_score AS total_goals, away_team AS team
+		FROM WorldCupProject..matches) AS sq
+GROUP BY team
+ORDER BY 2 DESC
